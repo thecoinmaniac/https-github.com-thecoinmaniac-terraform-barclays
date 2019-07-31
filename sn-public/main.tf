9a3a4b8f511541d732c5c88b252b6e1c743c242d
Goutham <arun.goutham@capgemini.com>
2019-07-31 12:59:56 +0530

    4

--- sn-public/main.tf
+++ sn-public/main.tf
@@ -8,3 +8,34 @@ resource "aws_subnet" "public" {
     Name = "public_${var.subnet_name}"
   }
 }
+
+## Internet gateway
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = "${var.vpc_id}"
+
+  tags = {
+    Name = "public_${var.subnet_name}"
+  }
+}
+
+## Routing table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = "${var.vpc_id}"
+
+  tags = {
+    Name = "public_${var.subnet_name}"
+  }
+}
+
+resource "aws_route" "gateway_route" {
+  route_table_id         = "${aws_route_table.public_route_table.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.internet_gateway.id}"
+}
+
+## Associate the routing table to public subnet
+resource "aws_route_table_association" "rt_assn" {
+  subnet_id      = "${aws_subnet.public.id}"
+  route_table_id = "${aws_route_table.public_route_table.id}"
+}
+
