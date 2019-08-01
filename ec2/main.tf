bfc185987e0c3613a7a24e86577855ac6d436bf3
Goutham <arun.goutham@capgemini.com>
2019-08-01 11:58:55 +0530

    13

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "ec2" {
   ami                         = "${var.ami_name}"
   instance_type               = "${var.instance_type}"
+  subnet_id                   = "${var.subnet_id}"
   associate_public_ip_address = true
 }
---
+++ ec2/variables.tf
@@ -0,0 +1,5 @@
+variable "ami_name" {}
+
+variable "instance_type" {}
+variable "subnet_id" {}
+
